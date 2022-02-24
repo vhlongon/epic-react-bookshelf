91580b8f085767949350b6cf2aeea8c64f5053e3
victor.longon <vhlongon@gmail.com>
2022-02-24 21:22:50 +0100

    finish exercise 10 - render as you fetch - resolver exercise and all extras

--- src/context/auth-context.exercise.js
+++ src/context/auth-context.exercise.js
@@ -2,22 +2,32 @@
 import {jsx} from '@emotion/core'
 
 import * as React from 'react'
+import {queryCache} from 'react-query'
 import * as auth from 'auth-provider'
 import {client} from 'utils/api-client'
 import {useAsync} from 'utils/hooks'
+import {setQueryDataForBook} from 'utils/books'
 import {FullPageSpinner, FullPageErrorFallback} from 'components/lib'
 
-async function getUser() {
+async function bootstrapAppData() {
   let user = null
 
   const token = await auth.getToken()
   if (token) {
-    const data = await client('me', {token})
+    const data = await client('bootstrap', {token})
+    queryCache.setQueryData('list-items', data.listItems, {
+      staleTime: 5000,
+    })
+    // Let's also set the books in the query cache as well
+    for (const listItem of data.listItems) {
+      setQueryDataForBook(listItem.book)
+    }
     user = data.user
   }
-
   return user
 }
+
+const appDataPromise = bootstrapAppData()
 
 const AuthContext = React.createContext()
 AuthContext.displayName = 'AuthContext'
@@ -36,15 +46,7 @@ function AuthProvider(props) {
   } = useAsync()
 
   React.useEffect(() => {
-    // we need to call getUser() sooner.
-    // 🐨 move the next line to just outside the AuthProvider
-    // 🦉 this means that as soon as this module is imported,
-    // it will start requesting the user's data so we don't
-    // have to wait until the app mounts before we kick off
-    // the request.
-    // We're moving from "Fetch on render" to "Render WHILE you fetch"!
-    const userPromise = getUser()
-    run(userPromise)
+    run(appDataPromise)
   }, [run])
 
   const login = React.useCallback(
@@ -60,12 +62,10 @@ function AuthProvider(props) {
     setData(null)
   }, [setData])
 
-  const value = React.useMemo(() => ({user, login, logout, register}), [
-    login,
-    logout,
-    register,
-    user,
-  ])
+  const value = React.useMemo(
+    () => ({user, login, logout, register}),
+    [login, logout, register, user],
+  )
 
   if (isLoading || isIdle) {
     return <FullPageSpinner />
