e340df1558eab3dd313325df5dcc9bc211bbc01f
victor.longon <vhlongon@gmail.com>
2022-02-24 22:14:26 +0100

    finish exercise 11 - unit tests - resolver exercise

--- src/utils/__tests__/misc.exercise.js
+++ src/utils/__tests__/misc.exercise.js
@@ -1,2 +1,5 @@
-test.todo('formatDate formats the date to look nice')
+import {formatDate} from '../misc'
 
+test('formatDate formats the date to look nice', () => {
+  expect(formatDate(new Date(2019, 0, 1))).toBe('Jan 19')
+})
