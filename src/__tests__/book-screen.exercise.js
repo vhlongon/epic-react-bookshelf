1972a699416ffb3d723d780c4a24ca6cbccc4d8d
victor.longon <vhlongon@gmail.com>
2022-02-28 20:50:11 +0100

    finish exercise 13 - integration testings - extras 1 and 2

--- src/__tests__/book-screen.exercise.js
+++ src/__tests__/book-screen.exercise.js
@@ -1,21 +1,22 @@
 import * as React from 'react'
-import {
-  render,
-  screen,
-  waitFor,
-  waitForElementToBeRemoved,
-} from '@testing-library/react'
+import {render, screen, waitForElementToBeRemoved} from '@testing-library/react'
 import {queryCache} from 'react-query'
 import {buildUser, buildBook} from 'test/generate'
 import * as auth from 'auth-provider'
+import * as usersDB from 'test/data/users'
+import * as booksDB from 'test/data/books'
+import * as listItemsDB from 'test/data/list-items'
 import {AppProviders} from 'context'
 import {App} from 'app'
-
-// 🐨 after each test, clear the queryCache and auth.logout
+import userEvent from '@testing-library/user-event'
+import {formatDate} from 'utils/misc'
 
 afterEach(async () => {
   queryCache.clear()
   await auth.logout()
+  await usersDB.reset()
+  await booksDB.reset()
+  await listItemsDB.reset()
 })
 
 test('renders all the book information', async () => {
@@ -23,32 +24,22 @@ test('renders all the book information', async () => {
   window.localStorage.setItem(auth.localStorageKey, fakeToken)
 
   const user = buildUser()
-  const book = buildBook()
+  await usersDB.create(user)
+  const authUser = await usersDB.authenticate(user)
+  window.localStorage.setItem(auth.localStorageKey, authUser.token)
+
+  const book = await booksDB.create(buildBook())
   const route = `/book/${book.id}`
   window.history.pushState({}, 'books', route)
-
-  const originalFetch = window.fetch
-
-  window.fetch = async (url, config) => {
-    if (url.endsWith('/bootstrap')) {
-      return {
-        ok: true,
-        json: async () => ({
-          user: {...user, token: fakeToken},
-          listItems: [],
-        }),
-      }
-    } else if (url.endsWith(`/books/${book.id}`)) {
-      return {ok: true, json: async () => ({book})}
-    }
-    return originalFetch(url, config)
-  }
 
   render(<App />, {
     wrapper: AppProviders,
   })
 
-  await waitForElementToBeRemoved(() => screen.getByLabelText(/loading/i))
+  await waitForElementToBeRemoved(() => [
+    ...screen.queryAllByLabelText(/loading/i),
+    ...screen.queryAllByAltText(/loading/i),
+  ])
 
   expect(screen.getByText(book.title)).toBeInTheDocument()
   expect(screen.getByText(book.author)).toBeInTheDocument()
@@ -58,4 +49,45 @@ test('renders all the book information', async () => {
     'src',
     book.coverImageUrl,
   )
+})
+
+test('can create a list item for the book', async () => {
+  const fakeToken = 'fakeToken'
+  window.localStorage.setItem(auth.localStorageKey, fakeToken)
+
+  const user = buildUser()
+  await usersDB.create(user)
+  const authUser = await usersDB.authenticate(user)
+  window.localStorage.setItem(auth.localStorageKey, authUser.token)
+
+  const book = await booksDB.create(buildBook())
+  const route = `/book/${book.id}`
+  window.history.pushState({}, 'books', route)
+
+  render(<App />, {
+    wrapper: AppProviders,
+  })
+
+  await waitForElementToBeRemoved(() => [
+    ...screen.queryAllByLabelText(/loading/i),
+    ...screen.queryAllByAltText(/loading/i),
+  ])
+
+  expect(screen.queryByText(/mark as read/i)).not.toBeInTheDocument()
+  expect(screen.queryByText(/remove from list/i)).not.toBeInTheDocument()
+  userEvent.click(screen.getByRole('button', {name: /add to list/i}))
+
+  await waitForElementToBeRemoved(() => [
+    ...screen.queryAllByLabelText(/loading/i),
+    ...screen.queryAllByAltText(/loading/i),
+  ])
+
+  expect(screen.getByText(formatDate(new Date()))).toBeInTheDocument()
+  expect(
+    screen.getByRole('button', {name: /mark as read/i}),
+  ).toBeInTheDocument()
+  expect(
+    screen.getByRole('button', {name: /remove from list/i}),
+  ).toBeInTheDocument()
+  expect(screen.queryByText(/add to list/i)).not.toBeInTheDocument()
 })
